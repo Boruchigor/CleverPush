1d2350f850a723022619954b420ef993a1d3a83a
Boruchigor <72554153+Boruchigor@users.noreply.github.com>
2021-11-30 12:22:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from 'react';
+import CleverPush from 'cleverpush';
 import './App.css';
 
 function App() {
-  CleverPush = window.CleverPush || [];
-  CleverPush.push(['triggerOptIn', function(err, subscriptionId) {
-      if (err) {
-          console.error(err);
-      } else {
-          console.log('successfully subscribed with id', subscriptionId);
-      }
-  }]);
-  
-  // Set the second parameter to 'true' to skip the opt-in timeout.
-  // It will also show the opt-in if the user has previously denied or unsubscribed.
-  CleverPush.push(['triggerOptIn', true]);
+
   return (
     <main>
       <h1>Welcome to CleverPush!</h1>
