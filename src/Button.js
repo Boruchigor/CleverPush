27a9fc9f729f0ebde4fa2d83e1882982a27fec2f
Boruchigor <72554153+Boruchigor@users.noreply.github.com>
2021-12-01 12:13:59 +0100

    Updated with iframe

--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const Button = ({text}) => {
-    return (
-        <div data-type="button-simple" data-button-background-color="#42a2f2" data-button-color="#ffffff" className="cleverpush-content-button" data-button-text={text}>
-            
-        </div>
-    )
-}
+const Button = ({ text }) => {
+  return (
+    <div id="parent">
+      <iframe src="https://example.com"></iframe>
+      <button
+        data-type="button-simple"
+        data-button-background-color="#42a2f2"
+        data-button-color="#ffffff"
+        className="cleverpush-content-button"
+        data-button-text={text}
+      />
+    </div>
+  );
+};
 
-export default Button 
+export default Button;
